e85b032ef210008e113ffecf7854bf81854845bc
Subhodip Roy <subhodiproy281@gmail.com>
2023-10-26 10:18:23 +0530

    more faster performance

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const { createAudioFile, getAudioBuffer } = require('simple-tts-mp3')
+// const {translate} = require('@vitalets/google-translate-api');
+const translate = require('@iamtraction/google-translate');
 const fs = require('fs');
 const path = require('path');
-const fileupload = require("express-fileupload");
 const { OpenAI } = require('openai');
 require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }))
-app.use(fileupload());
 app.use(express.json());
 
 const openai = new OpenAI({
@@ -25,44 +25,29 @@ app.get('/speak', (req, res) => {
     res.render('speak');
 });
 
-// app.post('/save-audio', (req, res) => {
-//     const audioData = req.files;
-//     console.log(audioData)
-//     // Assuming you send the audio data from the client
-//     // const filePath = path.join(__dirname, 'public', 'recorded-audio.wav');
-
-//     // fs.writeFile(filePath, audioData, (err) => {
-//     //   if (err) {
-//     //     console.error('Error saving audio:', err);
-//     //     res.status(500).send('Error saving audio');
-//     //   } else {
-//     //     res.status(200).send('Audio saved successfully');
-//     //   }
-//     // });
-//     res.json({ success: true })
-// });
-
 app.get('/qn-openai', async (req, res) => {
+    console.log(2);
     const question = req.query.qn;
-    const context = "Answer in same language as input question and in 1-2 sentences and in simple language. Act as a Doctor."
+    const bnToEng = await translate(question, { from: 'bn', to: 'en', raw: false });
+    const context = "Answer in simple language within 1-2 sentences. Act that you are a Doctor."
     // ask the question to openai
     const chatCompletion = await openai.chat.completions.create({
         // give the question and context to openai
         model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: `${question} ${context}` }],
+        messages: [{ role: 'system', content: `${context}` }, { role: 'user', content: `${bnToEng.text}` }],
     });
+    console.log(3);
     const GPT3Answer = chatCompletion.choices[0].message.content;
-    res.redirect('/save-audio?openaians=' + GPT3Answer)
+    const engToBn = await translate(GPT3Answer, { from: 'en', to: 'bn' });
+    res.redirect('/save-audio?openaians=' + engToBn.text)
 })
 
 app.get('/save-audio', async (req, res) => {
+    console.log(4);
     const answer = req.query.openaians;
     console.log(answer)
-    // await createAudioFile(answer, 'public/hello-world', 'bn')
     const audioBuffer = await getAudioBuffer(answer, 'bn');
-    // // wait for few seconds
-    // await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(audioBuffer);
+    console.log(5);
     res.render('listen', { audioBuffer: audioBuffer });
 });
 
